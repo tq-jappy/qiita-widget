703096f3ecec5fd5694653277147f7dbec19924f
Takumi Endo <tq-ed@v7.com>
2014-06-04 05:51:58 +0900

    styleにfont-family指定。その影響でscript.jsがエラーになるので、圧縮しないようにした。

--- compile.php
+++ compile.php
@@ -2,6 +2,7 @@
 <?php
 
 $css = trim(shell_exec('recess --compress ./Source/style.less'));
-$script = trim(shell_exec('coffee -p -c ./Source/script.coffee  | uglifyjs -nc'));
+# $script = trim(shell_exec('coffee -p -c ./Source/script.coffee  | uglifyjs -nc'));
+$script = trim(shell_exec('coffee -p -c ./Source/script.coffee'));
 $script = str_replace('<!--%css%-->', $css, $script);
 echo $script;
